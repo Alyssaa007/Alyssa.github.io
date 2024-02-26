2ba46de47307979e9c53f6513b411411b5723635
Alyssaa007 <a02lyssa@hotmail.com>
2024-02-26 17:46:25 -0500

    Update main.js

--- Silly-Story-Generator/main.js
+++ Silly-Story-Generator/main.js
@@ -33,11 +33,13 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = '${Math.round(300 / 14)} Stone';
+    const temperature =  '${Math.round((94 - 32)* (5/9))} centigrade';
+    newStory = newStory
+                .replace('300 pounds', weight)
+                .replace('94 fahrenheit', temperature);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
