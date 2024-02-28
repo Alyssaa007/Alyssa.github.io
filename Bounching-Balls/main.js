48b9f7c0564b81f9dcfaf4ea6eb9549664e4d0bf
Alyssaa007 <a02lyssa@hotmail.com>
2024-02-28 16:37:19 -0500

    mine isnt working the same so i looked at source code to see where i messed up. There is still more tho

--- Bounching-Balls/main.js
+++ Bounching-Balls/main.js
@@ -35,18 +35,18 @@ class Ball {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        ctx.fill();
+        ctx.fill( );
     }
 
     update() {
         if ((this.x + this.size) >= width) {
-            this.velX = -(this.velX);
+            this.velX = -(Math.abs(this.velX));
         }
         if ((this.x - this.size) <= 0) {
-            this.velY = -(this.velY);
+            this.velY = (Math.abs(this.velY));
         }
-        if ((this.y - this.size) <= 0) {
-            this.velY = -(this.velY);
+        if ((this.y - this.size) <= height) {
+            this.velY = Math.abs(this.velY);
         }
         this.x += this.velX;
         this.y += this.velY;
