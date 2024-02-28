40fc17d426575cf85c5dcbcd6e453561f37ab50c
Alyssaa007 <a02lyssa@hotmail.com>
2024-02-28 11:46:17 -0500

    i needed to add an s at the end of image

--- Image-Gallery/main.js
+++ Image-Gallery/main.js
@@ -6,29 +6,42 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const image = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
-const alts = {
-    'pic1.jpg':'Human eye close up',
-     'pic2.jpg': 'Rock that resembles a wave',
-      'pic3.jpg': 'Purple and white flowers',
-       'pic4.jpg': 'Hieroglyphics of pharaoh', 
-       'pic5.jpg': 'Moth on a leaf'
-}
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 
-for (const image of image) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${image}`);
-    newImage.setAttribute('alt', alts[image]);
-    
+const alts = {
+    'pic1.jpg':'Human eye close up',
+    'pic2.jpg': 'Rock that resembles a wave',
+    'pic3.jpg': 'Purple and white flowers',
+    'pic4.jpg': 'Hieroglyphics of pharaoh', 
+    'pic5.jpg': 'Moth on a leaf'
 }
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt =e.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
