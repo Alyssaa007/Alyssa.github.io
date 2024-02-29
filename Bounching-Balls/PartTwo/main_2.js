9a6c34bde49165f80b9c16f51c95cb9e2b88953a
Alyssaa007 <a02lyssa@hotmail.com>
2024-02-28 21:57:29 -0500

    Think ive added everything? i will check over becuase its not showing up the page

--- Bounching-Balls/PartTwo/main_2.js
+++ Bounching-Balls/PartTwo/main_2.js
@@ -139,14 +139,13 @@ collision() {
             const distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance < this.size + ball.size) {
-                ball.exists
+                ball.exists = false;
+                count--;
+                para.textContent = 'Ball Count' + count;
             }
         }
     }
 }
-
-
-
 
 
 // Populate the ball array.
@@ -168,20 +167,29 @@ while(balls.length < 25) {
     );
 
     balls.push(ball);
+    count++;
+    para.textContent = 'Ball count: ' + count;
 }
 
-// Loop function for the balls animation.
+// Loop function for the balls animation. Adds for the evil ball.
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 
 function loop() {
     ctx.fillStyle = "rgb(0, 0, 0, 0.25)";
     ctx.fillRect(0, 0, width, height);
 
     for (const ball of balls) {
-        ball.draw();
-        ball.update();
-        ball.collision();
+        if (ball.exists) {
+            ball.draw();
+            ball.update();
+            ball.collision();
+        }
     }
 
+    evilBall.draw();
+    evilBall.checkBounds();
+    evilBall.collision();
     requestAnimationFrame(loop);
 }
 
